d103150b78c183949ab05d0765cb520f82ea2e58
Victor Setubal <victordhlucas@gmail.com>
2025-07-02 07:53:48 -0300

    Update manage.py

--- T2/manage.py
+++ T2/manage.py
@@ -1,30 +1,25 @@
 from flask import Flask
-from flask_migrate import Migrate, MigrateCommand
-from flask.cli import with_appcontext
+from flask_migrate import Migrate
 from app import create_app, db
 import click
 
 app = create_app()
 migrate = Migrate(app, db)
 
-# Expor o comando flask db corretamente
 @app.cli.command("db-init")
-@with_appcontext
 def db_init():
     """Inicializa a pasta de migração"""
     from flask_migrate import init
     init()
 
 @app.cli.command("db-migrate")
-@with_appcontext
 @click.argument("message")
 def db_migrate(message):
     """Cria uma migração com mensagem"""
-    from flask_migrate import migrate
-    migrate(message=message)
+    from flask_migrate import migrate as _migrate
+    _migrate(message=message)
 
 @app.cli.command("db-upgrade")
-@with_appcontext
 def db_upgrade():
     """Aplica as migrações"""
     from flask_migrate import upgrade
